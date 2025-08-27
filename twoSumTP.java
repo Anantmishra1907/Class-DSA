abc7d61172f584c416d4e538aad4b210dc731da4
Anant Mishra <anantmishra1907@gmail.com>
2025-08-27 10:21:34 +0530

    Add a third approach

---
+++ twoSumTP.java
@@ -0,0 +1,30 @@
+public class twoSumTP {
+    public int[] findTwoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;    
+     while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left, right};  
+            } else if (sum < target) {
+                left++;
+            } else {       
+                right--;
+                }
+            }
+        return new int[]{-1, -1}; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        twoSumTP twoSum = new twoSumTP();
+        int[] result = twoSum.findTwoSum(nums, target);
+        if (result[0] != -1) {
+            System.out.println( result[0] + " and " + result[1]);
+        } else {
+            System.out.println("No two numbers add up to " + target);
+        }
+    }
+    
+}
